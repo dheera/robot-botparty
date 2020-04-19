8b21031990fac7c1ef480a633403e22a65bbf038
Ubuntu <ubuntu@ip-172-31-17-57.us-west-1.compute.internal>
2020-04-19 00:07:58 +0000

    imuNode

--- server/static/robot/js/imuNode.js
+++ server/static/robot/js/imuNode.js
@@ -1,30 +1,51 @@
 (()=>{
   let nh = ros.initNode("imu_node");
   let pub_imu_data = nh.advertise("/imu/data", "sensor_msgs/Imu");
+  let msg = { "@type": "sensor_msgs/Imu" };
 
-  let imu = new AbsoluteOrientationSensor({
+  let gyroscopeSensor = new Gyroscope({
     frequency: 60,
     referenceFrame: "device"
   });
 
-  imu.addEventListener("reading", e => {
-    let msg = { "@type": "sensor_msgs/Imu" };
-    msg.orientation = {
-      x: imu.quaternion[0],
-      y: imu.quaternion[1],
-      z: imu.quaternion[2],
-      w: imu.quaternion[3],
-    };
+  let linearAccelerationSensor = new LinearAccelerationSensor({
+    frequency: 60,
+    referenceFrame: "device"
+  });
+
+  let orientationSensor = new AbsoluteOrientationSensor({
+    frequency: 60,
+    referenceFrame: "device"
+  });
+
+  gyroscopeSensor.addEventListener("reading", e => {
     msg.angular_velocity = {
-      x: 0,
-      y: 0,
-      z: 0,
+      x: gyroscopeSensor.x,
+      y: gyroscopeSensor.y,
+      z: gyroscopeSensor.z,
     };
+  });
+
+  linearAccelerationSensor.addEventListener("reading", e => {
     msg.linear_acceleration = {
-      x: 0,
-      y: 0,
-      z: 0,
+      x: linearAccelerationSensor.x,
+      y: linearAccelerationSensor.y,
+      z: linearAccelerationSensor.z,
+    };
+  });
+
+  orientationSensor.addEventListener("reading", e => {
+    msg.orientation = {
+      x: orientationSensor.quaternion[0],
+      y: orientationSensor.quaternion[1],
+      z: orientationSensor.quaternion[2],
+      w: orientationSensor.quaternion[3],
     };
     pub_imu_data.publish(msg);
-  })
+  });
+
+  gyroscopeSensor.start();
+  linearAccelerationSensor.start();
+  orientationSensor.start();
+
 })();
